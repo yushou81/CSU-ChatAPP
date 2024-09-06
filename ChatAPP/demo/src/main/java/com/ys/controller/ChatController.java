f100885cd893546ab06ef443bc05e5128c70edd3
zyy <203423469@qq.com>
2024-09-06 12:46:11 +0800

    3

--- ChatAPP/demo/src/main/java/com/ys/controller/ChatController.java
+++ ChatAPP/demo/src/main/java/com/ys/controller/ChatController.java
@@ -12,45 +12,45 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.control.Separator;
 public class ChatController {
 
-    @FXML
-    private ListView<?> contractList; // 可以将泛型类型替换为实际的数据类型，如 ListView<String>。
-    @FXML
-    private StackPane nameTitle;
-    @FXML
-    private TextArea textArea;
-    @FXML
-    private Separator separator;
-    @FXML
-    private Button sendMessageButton;
-
-    @FXML
-    public void initialize() {
-        // 初始化控件或绑定数据
-        sendMessageButton.setOnAction(event -> handleSendMessage());
-    }
-
-    private void handleSendMessage() {
-        // 处理发送消息的逻辑
-        String message = textArea.getText(); // 获取文本区中的文本
-        if (!message.isEmpty()) {
-            System.out.println("Message sent: " + message);
-            textArea.clear(); // 清空文本区
-        } else {
-            System.out.println("Message cannot be empty.");
-        }
-    }
-
-
-
-
-    //更新头像
-    private void updateAvatar(){
-
-    }
-    //发送信息
-    private void sendMessage(){
-
-    }
+//    @FXML
+//    private ListView<?> contractList; // 可以将泛型类型替换为实际的数据类型，如 ListView<String>。
+//    @FXML
+//    private StackPane nameTitle;
+//    @FXML
+//    private TextArea textArea;
+//    @FXML
+//    private Separator separator;
+//    @FXML
+//    private Button sendMessageButton;
+//
+//    @FXML
+//    public void initialize() {
+//        // 初始化控件或绑定数据
+//        sendMessageButton.setOnAction(event -> handleSendMessage());
+//    }
+//
+//    private void handleSendMessage() {
+//        // 处理发送消息的逻辑
+//        String message = textArea.getText(); // 获取文本区中的文本
+//        if (!message.isEmpty()) {
+//            System.out.println("Message sent: " + message);
+//            textArea.clear(); // 清空文本区
+//        } else {
+//            System.out.println("Message cannot be empty.");
+//        }
+//    }
+//
+//
+//
+//
+//    //更新头像
+//    private void updateAvatar(){
+//
+//    }
+//    //发送信息
+//    private void sendMessage(){
+//
+//    }
 
 
 
