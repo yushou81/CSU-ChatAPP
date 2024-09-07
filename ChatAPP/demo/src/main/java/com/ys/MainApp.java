7244843e36f50c9e69693e20cca9ad39568ed8cd
lixingdada <2170423283@qq.com>
2024-09-07 18:45:08 +0800

    d

--- ChatAPP/demo/src/main/java/com/ys/MainApp.java
+++ ChatAPP/demo/src/main/java/com/ys/MainApp.java
@@ -44,7 +44,7 @@ public class MainApp extends Application {
 
 
             // 加载 FXML 文件
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/setting.fxml"));
 
 
             Parent root = loader.load();
