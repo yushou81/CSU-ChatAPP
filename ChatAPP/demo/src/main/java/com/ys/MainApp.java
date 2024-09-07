102f86b4180132b8b87dbed078becaf1a2b4b2a5
Yushou <3337544217@qq.com>
2024-09-07 18:50:33 +0800

    保存一下

--- ChatAPP/demo/src/main/java/com/ys/MainApp.java
+++ ChatAPP/demo/src/main/java/com/ys/MainApp.java
@@ -32,7 +32,7 @@ public class MainApp extends Application {
         // 初始化客户端并连接服务器
 
         client = new Client();
-        client.connect("192.168.221.164", 8080);  // 替换为你的服务器IP和端口号
+        client.connect("100.64.83.48", 8080);  // 替换为你的服务器IP和端口号
 //        100.64.83.48
 //        192.168.221.164
         ClientManager.setClient(client);
